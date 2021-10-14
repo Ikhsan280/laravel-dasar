fda2a7f66065bb07cd35fce2fdf9133dd4f49991
Ikhsan280 <ikhsan123ramadan@gmail.com>
2021-10-14 16:32:51 +0700

    dasar laravel

--- routes/web.php
+++ routes/web.php
@@ -24,22 +24,58 @@ route::get('/about', function () {
     . 'Laravel, emang keren.';
 });
 
-Route::get('/profil', function () {
-    return view('profil');
+Route::get('profil', function () {
+    return view('ikhsan/profil');
 });
 
-Route::get('/hobi', function () {
-    return view('hobi');
+Route::get('hobi', function () {
+    return view('ikhsan/hobi');
 });
 
-Route::get('/suka', function () {
-    return view('kesukaan');
+Route::get('suka', function () {
+    return view('ikhsan/kesukaan');
 });
 
-Route::get('/tentang', function () {
-    return view('tentang');
+Route::get('tentang', function () {
+    return view('ikhsan/tentang');
 });
 
-Route::get('/tujuan', function () {
-    return view('tujuan');
+Route::get('tujuan', function () {
+    return view('ikhsan/tujuan');
+});
+
+Route::get('index', function () {
+    return view('ikhsan/index');
+});
+
+Route::get('page/{page?}', function ($hal = 2) {
+    return "Halaman <b> $hal </b>";
+});
+
+Route::get('user/{name?}', function ($nama = "Ikhsan") {
+    return "Nama saya <b>$nama</b>";
+});
+
+Route::get('pesan/{makanan?}/{minuman?}/{cemilan?}', function ($makanan = null,$minuman = null,$cemilan = null) {
+    if ($makanan == null && $minuman == null && $cemilan == null) {
+        $s = "Anda Tidak Pesan, Silahkan Pulang";
+    }
+            if ($makanan != null ) {
+            $s = "Pesanan <br>"
+            . "anda pesan makan :<b> $makanan </b> <br>";
+        }
+        if ($makanan != null && $minuman != null ) {
+            $s = "Pesanan <br>"
+            . "anda pesan makan :<b> $makanan </b><br>"
+            . "anda pesan minum :<b> $minuman </b><br>";
+        }
+
+        if ($makanan != null && $minuman != null && $cemilan != null) {
+            $s = "Pesanan <br>"
+            . "anda pesan makan : <b>$makanan </b><br>"
+            . "anda pesan minum : <b>$minuman </b><br>"
+            . "anda pesan cemilan : <b>$cemilan</b>";
+}
+
+    return "$s";
 });
